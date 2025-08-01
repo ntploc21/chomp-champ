573570eac1868980da5817e529dbf56c2fc1d93e
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-08-01 16:23:43 +0700

    MGAME-92: Fix UI elements not update correctly.

--- Assets/Scripts/Reach UI/HUD/HUDManager.cs
+++ Assets/Scripts/Reach UI/HUD/HUDManager.cs
@@ -44,6 +44,7 @@ namespace Michsky.UI.Reach
                 return;
             }
 
+            GameDataManager gameDataManager = GUIManager.Instance.GameDataManager;
             SpawnManager spawnManager = FindObjectOfType<SpawnManager>();
 
             if (spawnManager != null)
@@ -206,6 +207,17 @@ namespace Michsky.UI.Reach
                 gameDataManager.OnDataChanged.AddListener(OnDataChanged);
                 // gameDataManager.OnScoreChanged.AddListener(OnScoreChanged);
                 // gameDataManager.OnLivesChanged.AddListener(OnLivesChanged);
+
+                // Set the initial state of the HUD
+                if (livesText != null)
+                {
+                    Debug.Log($"HUDManager: Setting initial lives text to {gameDataManager.SessionData.lives}");
+                    livesText.text = $"{gameDataManager.SessionData.lives}";
+                }
+                if (scoreText != null)
+                {
+                    scoreText.text = $"Score: {(int)gameDataManager.SessionData.score}";
+                }
             }
         }
 
